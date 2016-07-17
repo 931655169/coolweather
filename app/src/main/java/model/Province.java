edbe37589a07b8fc86ed7b0ec4b5c80a698ef205
ZJM <931655169@qq.com>
2016-07-17 15:23:00 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/model/Province.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by zjm on 2016/7/17.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceCode;
+    }
+    public void getProvinceCode(String provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
